9e970e31a47aadb002bc17962296b41e0a0335a2
djumanov <djumanovdev@gmail.com>
2025-07-30 12:22:51 +0500

    init

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Odoo18 Custom Dynamic Accounting Reports',
+    'version': '1.0.0',
+    'category': 'Accounting',
+    'depends': ['dynamic_accounts_report'],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
